fe383808f9bc02368e951de553eb807d2aff5196
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2024-01-04 01:19:26 -0300

    finalizacao_css_js_html

--- scripts/home.js
+++ scripts/home.js
@@ -11,10 +11,20 @@ function displayUserData(){
     const imageElement = document.getElementById('image');
     const nameElement = document.getElementById('name');
     const lastElement = document.getElementById('lastName');
-    
+    const idElement = document.getElementById('id');
+    const fullNameElement = document.getElementById('fullName');
+    const genderElement = document.getElementById('gender');
+    const hairElement = document.getElementById('hair');
+    const eyesElement = document.getElementById('eyes');
 
-    imageElement.src = data.image || '';
+    imageElement.src = data.image;
     nameElement.innerText = data.firstName;
     lastElement.innerText = data.lastName;
+    idElement.innerText = 'ID: ' + data.id;
+    fullNameElement.innerText = `${data.firstName} ${data.lastName}`;
+    genderElement.innerText = 'SEX:'+ data.gender;
+    hairElement.innerText = data.hair?.type ||'';
+    eyesElement.innerText = data.eyesColor  || '';
+
 }
 
